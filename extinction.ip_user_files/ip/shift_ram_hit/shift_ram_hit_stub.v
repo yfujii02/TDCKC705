99fda892d20a12c5978e6f9a4f0d670413327146
Yuki Fujii <yuki.fujii@monash.edu>
2021-02-04 14:04:36 +1100

    Add shift_ram_hit

---
+++ extinction.ip_user_files/ip/shift_ram_hit/shift_ram_hit_stub.v
@@ -0,0 +1,23 @@
+// Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2020.1 (win64) Build 2902540 Wed May 27 19:54:49 MDT 2020
+// Date        : Thu Jan 28 15:31:32 2021
+// Host        : dyna-comet running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               c:/Users/comet/Desktop/COTTRI_PROJECT/Firmware/Extinction2/TDCKC705/extinction.srcs/sources_1/ip/shift_ram_hit/shift_ram_hit_stub.v
+// Design      : shift_ram_hit
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7k325tffg900-2
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "c_shift_ram_v12_0_14,Vivado 2020.1" *)
+module shift_ram_hit(A, D, CLK, Q)
+/* synthesis syn_black_box black_box_pad_pin="A[7:0],D[0:0],CLK,Q[0:0]" */;
+  input [7:0]A;
+  input [0:0]D;
+  input CLK;
+  output [0:0]Q;
+endmodule
